dd68266c448be2a667490f873c19f345aa13055b
bharaths44 <bharathsatheesan@gmail.com>
2025-11-07 00:10:13 +0530

    Add GOOGLE_REDIRECT_URI and APP_AUTHORIZED_REDIRECT_URIS to environment variable configuration

--- src/main/java/com/bharath/incometer/IncometerApplication.java
+++ src/main/java/com/bharath/incometer/IncometerApplication.java
@@ -19,6 +19,8 @@ public class IncometerApplication {
 		System.setProperty("GEMINI_API_KEY", Objects.requireNonNull(dotenv.get("GEMINI_API_KEY")));
 		System.setProperty("GOOGLE_CLIENT_ID", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_ID")));
 		System.setProperty("GOOGLE_CLIENT_SECRET", Objects.requireNonNull(dotenv.get("GOOGLE_CLIENT_SECRET")));
+		System.setProperty("GOOGLE_REDIRECT_URI", Objects.requireNonNull(dotenv.get("GOOGLE_REDIRECT_URI")));
+		System.setProperty("APP_AUTHORIZED_REDIRECT_URIS", Objects.requireNonNull(dotenv.get("APP_AUTHORIZED_REDIRECT_URIS")));
 		SpringApplication.run(IncometerApplication.class, args);
 	}
 
